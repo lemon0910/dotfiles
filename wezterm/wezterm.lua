933ee8ed864395af25d0eabc4bea80dfe9999c19
lemon0910 <lemon9010@gmail.com>
2023-12-14 09:48:33 +0800

    更新 wezterm 配置

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,158 +1,85 @@
 local wezterm = require 'wezterm'
 
-return {
-  -- Smart tab bar [distraction-free mode]
-  hide_tab_bar_if_only_one_tab = true,
-
-  -- Color scheme
-  -- https://wezfurlong.org/wezterm/config/appearance.html
-
-
-  window_background_opacity = 0.95,
-  initial_cols = 150,
-  initial_rows = 40,
-
-  animation_fps = 60,
-   max_fps = 60,
-   front_end = 'WebGpu',
-   webgpu_power_preference = 'HighPerformance',
-
-  -- Font configuration
-  -- https://wezfurlong.org/wezterm/config/fonts.html
-  font = wezterm.font('Hack Nerd Font'),
-  font_size = 12.0,
-
-  -- color_scheme = 'Solarized (dark) (terminal.sexy)',
-  -- color_scheme = 'Solarized Darcula',
-  -- color_scheme = 'BlulocoDark',
-  -- default_prog = { 'zsh' },
-  color_scheme = 'tokyonight_moon',
-
-  window_frame = {
-      -- The font used in the tab bar.
-      -- Roboto Bold is the default; this font is bundled
-      -- with wezterm.
-      -- Whatever font is selected here, it will have the
-      -- main font setting appended to it to pick up any
-      -- fallback fonts you may have used there.
-      font = wezterm.font { family = 'Roboto', weight = 'Bold' },
-
-      -- The size of the font in the tab bar.
-      -- Default to 10.0 on Windows but 12.0 on other systems
-      font_size = 12.0,
-
-      -- The overall background color of the tab bar when
-      -- the window is focused
-      active_titlebar_bg = '#333333',
-
-      -- The overall background color of the tab bar when
-      -- the window is not focused
-      inactive_titlebar_bg = '#333333',
+-- Kanagawa with nice tab bars
+local kanagawa_custom = wezterm.color.get_builtin_schemes()["kanagawabones"]
+kanagawa_custom.tab_bar = {
+  background = "black",
+  active_tab = {
+    bg_color = "#1f1f28",
+    fg_color = "#dcd7ba",
   },
-
-  colors = {
-      tab_bar = {
-          -- The color of the strip that goes along the top of the window
-          -- (does not apply when fancy tab bar is in use)
-          background = '#0b0022',
-
-          -- The active tab is the one that has focus in the window
-          active_tab = {
-              -- The color of the background area for the tab
-              bg_color = '#2b2042',
-              -- The color of the text for the tab
-              fg_color = '#c0c0c0',
-
-              -- Specify whether you want "Half", "Normal" or "Bold" intensity for the
-              -- label shown for this tab.
-              -- The default is "Normal"
-              intensity = 'Normal',
-
-              -- Specify whether you want "None", "Single" or "Double" underline for
-              -- label shown for this tab.
-              -- The default is "None"
-              underline = 'None',
-
-              -- Specify whether you want the text to be italic (true) or not (false)
-              -- for this tab.  The default is false.
-              italic = false,
-
-              -- Specify whether you want the text to be rendered with strikethrough (true)
-              -- or not for this tab.  The default is false.
-              strikethrough = false,
-          },
-
-          -- Inactive tabs are the tabs that do not have focus
-          inactive_tab = {
-              bg_color = '#1b1032',
-              fg_color = '#808080',
-
-              -- The same options that were listed under the `active_tab` section above
-              -- can also be used for `inactive_tab`.
-          },
-
-          -- You can configure some alternate styling when the mouse pointer
-          -- moves over inactive tabs
-          inactive_tab_hover = {
-              bg_color = '#3b3052',
-              fg_color = '#909090',
-              italic = true,
-
-              -- The same options that were listed under the `active_tab` section above
-              -- can also be used for `inactive_tab_hover`.
-          },
-
-          -- The new tab button that let you create new tabs
-          new_tab = {
-              bg_color = '#1b1032',
-              fg_color = '#808080',
-
-              -- The same options that were listed under the `active_tab` section above
-              -- can also be used for `new_tab`.
-          },
-
-          -- You can configure some alternate styling when the mouse pointer
-          -- moves over the new tab button
-          new_tab_hover = {
-              bg_color = '#3b3052',
-              fg_color = '#909090',
-              italic = true,
-
-              -- The same options that were listed under the `active_tab` section above
-              -- can also be used for `new_tab_hover`.
-          },
-      },
+  inactive_tab = {
+    bg_color = "black",
+    fg_color = "79797c",
   },
-
-  -- window_background_image = '/Users/lemon/.config/wezterm/images/astro-jelly.jpg',
-  -- window_background_image_hsb = {},
-
-  -- Disable ligatures
-  -- https://wezfurlong.org/wezterm/config/font-shaping.html
-  -- harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
-
-  leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 },
-  keys = {
-    {
-      key = 'd',
-      mods = 'CMD',
-      action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
-    },
-    {
-      key = 'D',
-      mods = 'CMD',
-      action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
-    },
-    {
-        key = 'w',
-        mods = 'CMD',
-        action = wezterm.action.CloseCurrentPane { confirm = true },
-    },
-    {
-        key = 'b',
-        mods = 'CTRL',
-        action = wezterm.action.RotatePanes 'CounterClockwise',
-    },
-    { key = 'n', mods = 'CTRL', action = wezterm.action.RotatePanes 'Clockwise' },
+  new_tab = {
+    bg_color = "black",
+    fg_color = "79797c",
   },
 }
+
+-- Switch theme based on appearance
+local function colorscheme(appearance)
+  -- if appearance:find "Dark" then
+    -- return "kanagawa_custom"
+  -- else
+    -- return "Catppuccin Latte"
+  -- end
+  -- return "Gruvbox Material (Gogh)"
+  return "Tokyo Night Moon"
+end
+
+-- General config
+local config = {
+  color_schemes = {
+    ["kanagawa_custom"] = kanagawa_custom,
+  },
+  color_scheme = colorscheme(wezterm.gui.get_appearance()),
+  -- font = wezterm.font 'Hack Nerd Font',
+  font_size = 13,
+  -- line_height = 1.2,
+  -- cell_width = 0.9,
+  use_fancy_tab_bar = false,
+  force_reverse_video_cursor = true,
+  hide_tab_bar_if_only_one_tab = true,
+  adjust_window_size_when_changing_font_size = false,
+  max_fps = 120,
+  tab_max_width = 32,
+  window_decorations = "RESIZE",
+  window_background_opacity = 0.9,
+  enable_scroll_bar = true,
+  scrollback_lines = 500000,
+}
+
+local a = wezterm.action
+
+local function map(things)
+  local t = {}
+  for key in string.gmatch(things, '([^,]+)') do
+    table.insert(t, a.SendKey { key = key })
+  end
+  return t
+end
+
+config.keys = {
+  {
+    key = 'd',
+    mods = 'CMD',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 'd',
+    mods = 'CMD|SHIFT',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+  { key = 'n', mods = 'CMD', action = wezterm.action.RotatePanes 'Clockwise' },
+  { key = 'LeftArrow', mods = 'CMD', action = wezterm.action({ ActivateTabRelative = -1 }) },
+  { key = 'RightArrow', mods = 'CMD', action = wezterm.action({ ActivateTabRelative = 1 }) },
+  {
+    key = 'w',
+    mods = 'CMD',
+    action = wezterm.action.CloseCurrentPane { confirm = false },
+  },
+}
+
+return config
