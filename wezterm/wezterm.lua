69f61300d81b29395038e45bbc2caae77a1f560e
lemon0910 <lemon9010@gmail.com>
2024-03-11 14:09:44 +0800

    更新 wezterm 配置

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,65 +1,36 @@
 local wezterm = require 'wezterm'
 
--- Kanagawa with nice tab bars
-local kanagawa_custom = wezterm.color.get_builtin_schemes()["kanagawabones"]
-kanagawa_custom.tab_bar = {
-  background = "black",
-  active_tab = {
-    bg_color = "#1f1f28",
-    fg_color = "#dcd7ba",
-  },
-  inactive_tab = {
-    bg_color = "black",
-    fg_color = "79797c",
-  },
-  new_tab = {
-    bg_color = "black",
-    fg_color = "79797c",
-  },
-}
-
--- Switch theme based on appearance
-local function colorscheme(appearance)
-  -- if appearance:find "Dark" then
-    -- return "kanagawa_custom"
-  -- else
-    -- return "Catppuccin Latte"
-  -- end
-  -- return "Gruvbox Material (Gogh)"
-  return "Tokyo Night Moon"
-end
-
 -- General config
 local config = {
-  color_schemes = {
-    ["kanagawa_custom"] = kanagawa_custom,
-  },
-  color_scheme = colorscheme(wezterm.gui.get_appearance()),
-  -- font = wezterm.font 'Hack Nerd Font',
+  -- color_scheme = "Tokyo Night Moon",
+  color_scheme = "Catppuccin Mocha",
+  font = wezterm.font("Hack Nerd Font", { weight = "Regular" }),
   font_size = 13,
   -- line_height = 1.2,
   -- cell_width = 0.9,
   use_fancy_tab_bar = false,
-  force_reverse_video_cursor = true,
+  -- force_reverse_video_cursor = true,
   hide_tab_bar_if_only_one_tab = true,
   adjust_window_size_when_changing_font_size = false,
   max_fps = 120,
   tab_max_width = 32,
   window_decorations = "RESIZE",
-  window_background_opacity = 0.9,
   enable_scroll_bar = true,
   scrollback_lines = 500000,
+  show_new_tab_button_in_tab_bar = false,
+  window_background_opacity = 0.9,
+  macos_window_background_blur = 70,
+  text_background_opacity = 0.9,
+
+  window_padding = {
+    left = 20,
+    right = 20,
+    top = 20,
+    bottom = 5,
+  },
 }
 
 local a = wezterm.action
-
-local function map(things)
-  local t = {}
-  for key in string.gmatch(things, '([^,]+)') do
-    table.insert(t, a.SendKey { key = key })
-  end
-  return t
-end
 
 config.keys = {
   {
